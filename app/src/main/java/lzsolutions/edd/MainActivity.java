85a65720b1787ee3cacefa0ff6556961e06c4b13
Jerry <jaredlindenberg1@gmail.com>
2017-03-13 14:38:15 -0500

    Initial Project Upload

---
+++ app/src/main/java/lzsolutions/edd/MainActivity.java
@@ -0,0 +1,15 @@
+package lzsolutions.edd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// lz solutions is #1
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello World");
+    }
+}
